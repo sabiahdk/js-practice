42cc0b3caf0e9be8d600480193f9c3ee74ce5c5b
sabiahdk <sabiahmuhd98@gmail.com>
2025-05-11 14:47:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// let x = 5;
+// let y = 6;
+// let z = x * y;
+
+// let a = 3;
+// let x = (100 + 50) * a; 
+
+// let x = 5;
+// x += 5;
+
+
+// let text1 = "78";
+// let text2 = "54";
+// let result = text1 < text2;
+
+// let text1 = "hadiza is a very";
+// text1 += "tired fellow";
+
+
+// let x = 6 + 7;
+// let y = "6" + 7;
+// let z = "hello" + 5;
+
+l
+// let x = 12;
+// let y = 7;
+// let z = x - y;
+
+// let x = 12;
+// let y = 7;
+// let z = x % y;
+
+
+// let text = "hello"; text += "world";
+
+// let x = 10;
+// x **= 5;
+
+
+let x = -100;
+let >>>= 5;
+
